35d2d67745e8dc010958d925f17fc77a3b4704cd
jiangxiaohu <1299252485@qq.com>
2018-09-09 12:04:09 +0800

    Spring Boot Demo

---
+++ demo/src/main/java/com/jiangxiaohu/demo/Controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.jiangxiaohu.demo.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Administrator on 2018/9/9/009.
+ */
+@Controller
+@RequestMapping("home")
+public class HomeController {
+
+    @RequestMapping("admin")
+    public String getHome(){
+
+        return "home";
+    }
+}
